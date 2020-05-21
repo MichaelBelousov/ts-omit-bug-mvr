fc9d5aa69db278b4e6251ca638ad065c658dded2
Michael Belousov <michael.belousov98@gmail.com>
2020-05-21 13:46:10 -0400

    confirmed doesn't work in ~3.8, but it did in 3.9

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 "use strict";
 exports.__esModule = true;
-exports.myConstWithOmit2 = exports.myConstWithLibOmit = exports.myFuncWithOmit2 = exports.myFuncWithLibOmit = void 0;
 function myFuncWithLibOmit(param) { }
 exports.myFuncWithLibOmit = myFuncWithLibOmit;
 function myFuncWithOmit2(param) { }
