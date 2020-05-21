8cda1125656868d35ed59e71c53a83cef3c0d31f
Michael Belousov <michael.belousov98@gmail.com>
2020-05-21 13:41:33 -0400

    more minimal repro without more index signature types, just using partial

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,11 @@
 declare type Omit2<T, K extends keyof any, Keys extends keyof T = Exclude<keyof T, K>> = {
     [P in Keys]: T[P];
 };
-declare type MakeRequired<T, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>;
-declare type Rebound<Type extends object, NewThis> = {
-    [K in keyof Type]: Type[K] extends (...p: infer P) => infer R ? (this: NewThis, ...p: P) => R : Type[K];
-};
 import { Marker } from "@bentley/imodeljs-frontend";
-export declare type MyTypeWithLibOmit = Rebound<Omit<MakeRequired<Marker, "worldLocation">, "image"> & {
-    image: string;
-    other: number;
-}, Marker>;
-export declare type MyTypeWithOmit2 = Rebound<Omit2<MakeRequired<Marker, "worldLocation">, "image"> & {
-    image: string;
-    other: number;
-}, Marker>;
+export declare type MyTypeWithLibOmit = Partial<Omit<Marker, "">>;
+export declare type MyTypeWithOmit2 = Partial<Omit2<Marker, "">>;
 export declare function myFuncWithLibOmit(param: MyTypeWithLibOmit): void;
 export declare function myFuncWithOmit2(param: MyTypeWithOmit2): void;
-export declare const myConstWithLibOmit: (param: Rebound<Pick<MakeRequired<Marker, "worldLocation">, "visible" | "label" | "title" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration"> & {
-    image: string;
-    other: number;
-}, Marker>) => void;
-export declare const myConstWithOmit2: (param: Rebound<Omit2<MakeRequired<Marker, "worldLocation">, "image", "visible" | "label" | "title" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration"> & {
-    image: string;
-    other: number;
-}, Marker>) => void;
+export declare const myConstWithLibOmit: (param: Partial<Pick<Marker, "visible" | "label" | "title" | "image" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration">>) => void;
+export declare const myConstWithOmit2: (param: Partial<Omit2<Marker, "", "visible" | "label" | "title" | "image" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration">>) => void;
 export {};
--- index.ts
+++ index.ts
@@ -6,31 +6,10 @@ type Omit2<
   [P in Keys]: T[P];
 };
 
-type MakeRequired<T, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>;
-
-type Rebound<Type extends object, NewThis> = {
-  [K in keyof Type]: Type[K] extends (...p: infer P) => infer R
-    ? (this: NewThis, ...p: P) => R
-    : Type[K];
-};
-
 import { Marker } from "@bentley/imodeljs-frontend";
 
-export type MyTypeWithLibOmit = Rebound<
-  Omit<MakeRequired<Marker, "worldLocation">, "image"> & {
-    image: string;
-    other: number;
-  },
-  Marker
->;
-
-export type MyTypeWithOmit2 = Rebound<
-  Omit2<MakeRequired<Marker, "worldLocation">, "image"> & {
-    image: string;
-    other: number;
-  },
-  Marker
->;
+export type MyTypeWithLibOmit = Partial<Omit<Marker, "">>;
+export type MyTypeWithOmit2 = Partial<Omit2<Marker, "">>;
 
 export function myFuncWithLibOmit(param: MyTypeWithLibOmit) {}
 export function myFuncWithOmit2(param: MyTypeWithOmit2) {}
