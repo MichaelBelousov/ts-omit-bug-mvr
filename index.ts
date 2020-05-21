e2f3d88329569b5e58bcebb314ef7231abca70d9
Michael Belousov <michael.belousov98@gmail.com>
2020-05-21 14:02:57 -0400

    confirmed composition order does not matter, the output is still unusable in 3.8

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ type Partial2<T> = {
 import { Marker } from "@bentley/imodeljs-frontend";
 
 export type MyTypeWithLibOmit = Partial<Omit<Marker, "">>;
-export type MyTypeWithOmit2 = Partial2<Omit2<Marker, "">>;
+export type MyTypeWithOmit2 = Omit2<Partial2<Marker>, "">;
 
 export function myFuncWithLibOmit(param: MyTypeWithLibOmit) {}
 export function myFuncWithOmit2(param: MyTypeWithOmit2) {}
