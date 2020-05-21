ab31f5a9897c9d1fd26c68faccdd2d32a288db78
Michael Belousov <michael.belousov98@gmail.com>
2020-05-21 13:44:30 -0400

    confirmed it is fixed in typescript 3.9.3

--- index.d.ts
+++ index.d.ts
@@ -9,6 +9,6 @@ export declare type MyTypeWithLibOmit = Partial<Omit<Marker, "">>;
 export declare type MyTypeWithOmit2 = Partial2<Omit2<Marker, "">>;
 export declare function myFuncWithLibOmit(param: MyTypeWithLibOmit): void;
 export declare function myFuncWithOmit2(param: MyTypeWithOmit2): void;
-export declare const myConstWithLibOmit: (param: Partial<Pick<Marker, "visible" | "label" | "title" | "image" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration">>) => void;
-export declare const myConstWithOmit2: (param: Partial2<Omit2<Marker, "", "visible" | "label" | "title" | "image" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration">>) => void;
+export declare const myConstWithLibOmit: (param: MyTypeWithLibOmit) => void;
+export declare const myConstWithOmit2: (param: MyTypeWithOmit2) => void;
 export {};
