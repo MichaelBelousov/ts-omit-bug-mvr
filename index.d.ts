82629ef36bd2ef107bb1debb58f0ccb526a07f0c
Michael Belousov <michael.belousov98@gmail.com>
2020-05-21 13:30:41 -0400

    so far no reproduction even with complicated nested types similar to private repo

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,15 @@
 declare type Omit2<T, K extends keyof any, Keys extends keyof T = Exclude<keyof T, K>> = {
     [P in Keys]: T[P];
 };
+declare type MakeRequired<T, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>;
+declare type Rebound<Type extends object, NewThis> = {
+    [K in keyof Type]: Type[K] extends (...p: infer P) => infer R ? (this: NewThis, ...p: P) => R : Type[K];
+};
 import { Marker } from "@bentley/imodeljs-frontend";
-export declare type MyTypeWithLibOmit = Omit<Marker, "_scaleFactor" | "_isHilited">;
-export declare type MyTypeWithOmit2 = Omit2<Marker, "_scaleFactor" | "_isHilited">;
+export declare type MyTypeWithLibOmit = Rebound<Omit<MakeRequired<Marker, "worldLocation">, "image"> & {
+    image: string;
+}, Marker>;
+export declare type MyTypeWithOmit2 = Rebound<Omit2<MakeRequired<Marker, "worldLocation">, "image"> & {
+    image: string;
+}, Marker>;
 export {};
--- index.ts
+++ index.ts
@@ -1,9 +1,27 @@
+type Omit2<
+  T,
+  K extends keyof any,
+  Keys extends keyof T = Exclude<keyof T, K>
+> = {
+  [P in Keys]: T[P];
+};
 
-type Omit2<T, K extends keyof any, Keys extends keyof T = Exclude<keyof T, K>> = {
-    [P in Keys]: T[P];
+type MakeRequired<T, K extends keyof T> = Partial<Omit<T, K>> & Pick<T, K>;
+
+type Rebound<Type extends object, NewThis> = {
+  [K in keyof Type]: Type[K] extends (...p: infer P) => infer R
+    ? (this: NewThis, ...p: P) => R
+    : Type[K];
 };
 
 import { Marker } from "@bentley/imodeljs-frontend";
 
-export type MyTypeWithLibOmit = Omit<Marker, "_scaleFactor" | "_isHilited">;
-export type MyTypeWithOmit2 = Omit2<Marker, "_scaleFactor" | "_isHilited">;
+export type MyTypeWithLibOmit = Rebound<
+  Omit<MakeRequired<Marker, "worldLocation">, "image"> & { image: string },
+  Marker
+>;
+
+export type MyTypeWithOmit2 = Rebound<
+  Omit2<MakeRequired<Marker, "worldLocation">, "image"> & { image: string },
+  Marker
+>;
