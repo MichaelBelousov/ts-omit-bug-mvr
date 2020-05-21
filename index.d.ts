a004734e38122bd3607bb6d1d509add1613f3e1d
Michael Belousov <michael.belousov98@gmail.com>
2020-05-21 13:43:49 -0400

    tried with custom partial just in case

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,14 @@
 declare type Omit2<T, K extends keyof any, Keys extends keyof T = Exclude<keyof T, K>> = {
     [P in Keys]: T[P];
 };
+declare type Partial2<T> = {
+    [P in keyof T]?: T[P];
+};
 import { Marker } from "@bentley/imodeljs-frontend";
 export declare type MyTypeWithLibOmit = Partial<Omit<Marker, "">>;
-export declare type MyTypeWithOmit2 = Partial<Omit2<Marker, "">>;
+export declare type MyTypeWithOmit2 = Partial2<Omit2<Marker, "">>;
 export declare function myFuncWithLibOmit(param: MyTypeWithLibOmit): void;
 export declare function myFuncWithOmit2(param: MyTypeWithOmit2): void;
 export declare const myConstWithLibOmit: (param: Partial<Pick<Marker, "visible" | "label" | "title" | "image" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration">>) => void;
-export declare const myConstWithOmit2: (param: Partial<Omit2<Marker, "", "visible" | "label" | "title" | "image" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration">>) => void;
+export declare const myConstWithOmit2: (param: Partial2<Omit2<Marker, "", "visible" | "label" | "title" | "image" | "rect" | "worldLocation" | "size" | "position" | "imageOffset" | "imageSize" | "labelOffset" | "labelMaxWidth" | "labelColor" | "labelAlign" | "labelBaseline" | "labelFont" | "tooltipOptions" | "htmlElement" | "wantImage" | "drawFunc" | "onMouseEnter" | "onMouseLeave" | "onMouseMove" | "onMouseButton" | "pick" | "setScaleFactor" | "drawDecoration" | "setImage" | "setImageUrl" | "setPosition" | "addMarker" | "addDecoration">>) => void;
 export {};
--- index.ts
+++ index.ts
@@ -6,10 +6,14 @@ type Omit2<
   [P in Keys]: T[P];
 };
 
+type Partial2<T> = {
+  [P in keyof T]?: T[P];
+};
+
 import { Marker } from "@bentley/imodeljs-frontend";
 
 export type MyTypeWithLibOmit = Partial<Omit<Marker, "">>;
-export type MyTypeWithOmit2 = Partial<Omit2<Marker, "">>;
+export type MyTypeWithOmit2 = Partial2<Omit2<Marker, "">>;
 
 export function myFuncWithLibOmit(param: MyTypeWithLibOmit) {}
 export function myFuncWithOmit2(param: MyTypeWithOmit2) {}
